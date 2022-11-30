acfaa338494609cce34a61ccf9d72fd68d7e21c1
Ryan Miller <ryan.miller5466@gmail.com>
2022-11-29 22:14:30 -0500

    Reverts border-size changes on grid

--- foundations/etch-a-sketch/index.js
+++ foundations/etch-a-sketch/index.js
@@ -4,9 +4,9 @@ let reset = document.querySelector("#reset")
 
 let boardCopy = board;
 
-let bWidth = board.offsetWidth - 4;
-let bHeight = board.offsetHeight - 4;
-let boxesInLine = 25 ;
+let bWidth = board.offsetWidth;
+let bHeight = board.offsetHeight;
+let boxesInLine = 25;
 let boxSize = bWidth / boxesInLine;
 let totalBoxes = boxesInLine * boxesInLine; // (bWidth / boxesInLine) * (bHeight / boxesInLine) 
 
