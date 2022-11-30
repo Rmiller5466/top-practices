728c99c5f7e7c7b3ed3f7969e0e5d506d95687fd
Ryan Miller <ryan.miller5466@gmail.com>
2022-11-29 21:48:47 -0500

    Adds buttons for grid toggle and clearing board

--- foundations/etch-a-sketch/index.js
+++ foundations/etch-a-sketch/index.js
@@ -1,11 +1,14 @@
 let board = document.querySelector(".container");
-let boardCopy = board
+let gridToggle = document.querySelector("#grid-toggle")
+let reset = document.querySelector("#reset")
 
-let bWidth = board.offsetWidth;
-let bHeight = board.offsetHeight;
-let boxesInLine = 25 
-let boxSize = bWidth / boxesInLine
-let totalBoxes = boxesInLine * boxesInLine // (bWidth / boxesInLine) * (bHeight / boxesInLine) 
+let boardCopy = board;
+
+let bWidth = board.offsetWidth - 4;
+let bHeight = board.offsetHeight - 4;
+let boxesInLine = 25 ;
+let boxSize = bWidth / boxesInLine;
+let totalBoxes = boxesInLine * boxesInLine; // (bWidth / boxesInLine) * (bHeight / boxesInLine) 
 
 for (let i = 0; i < totalBoxes; i++){
     
@@ -14,9 +17,21 @@ for (let i = 0; i < totalBoxes; i++){
     box.addEventListener("mouseover", function() {
         box.style.backgroundColor = "green";
     });
-    box.style.width = `${boxSize}px`
-    box.style.height = `${boxSize}px`
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+
     board.appendChild(box);
-    
 };
 
+gridToggle.addEventListener('change', function() {
+    
+    for (const child of board.children) {
+        child.classList.toggle('grid');
+    }
+});
+
+reset.addEventListener('click', function() {
+    for (const child of board.children) {
+        child.style.backgroundColor = "blue";
+    }
+});
