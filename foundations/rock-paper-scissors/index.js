181502e1dcafb438d56b699627e0b92e9bee99b3
Ryan <ryan.miller5466@gmail.com>
2022-12-16 11:59:27 -0500

    Fixes game end, adds ability to restart
    - Game now properly stops at score of 5
    - Ability to restart the game
    - Refactors and cleans the code

--- foundations/rock-paper-scissors/index.js
+++ foundations/rock-paper-scissors/index.js
@@ -2,111 +2,147 @@ let buttonR = document.querySelector("#rock");
 let buttonP = document.querySelector("#paper");
 let buttonS = document.querySelector("#scissors");
 
-addListeners()
-
 let playerScore = document.querySelector("#ps");
-let playerScoreInt = 0;
 let playerChoice = document.querySelector("#pd");
 
 let computerScore = document.querySelector("#cs");
-let computerScoreInt = 0;
 let computerChoice = document.querySelector("#cd");
 
 let results = document.querySelector("#res");
 let cont = document.querySelector(".container");
 
-function addListeners(){
+let restartButton = document.createElement("button");
+restartButton.textContent = "Restart?";
+restartButton.addEventListener('click', () => restart());
 
-    buttonR.addEventListener('click', () => driver(buttonR.value));
-    buttonP.addEventListener('click', () => driver(buttonP.value));
-    buttonS.addEventListener('click', () => driver(buttonS.value));
+let endText = document.createElement("h2");
+endText.textContent = "Game is Over!";
+
+let playerScoreInt = 0;
+let computerScoreInt = 0;
+
+let theButtons = [buttonR,buttonP,buttonS];
+
+init();
+addListeners();
+
+function init(){
+    playerScoreInt = 0;
+    computerScoreInt = 0;
+
+    playerScore.textContent = "0";
+    computerScore.textContent = "0";
+
+    playerChoice.textContent = "";
+    computerChoice.textContent = "";
+    results.textContent = "";
+}
+
+function addListeners(){
+    for (let button of theButtons){
+        button.addEventListener('click', () => driver(button.value));
+    }
 }
 
 function driver(val){
-    //console.log("Welcome to Rock Paper Scissors!")
-    //playerScore = 0;
-    //computerScore = 0;
-    //console.log(`Starting round ${i + 1}`)
-    //console.log(`Player chose ${val}`)
     playerChoice.textContent = val;
     winner = decideWinner(val, getComputerChoice());
+
     switch(winner){
         case 1:
             results.textContent = "The player wins!";
             playerScoreInt++;
             playerScore.textContent = playerScoreInt;
-            break
+            break;
         case 2:
             results.textContent = "The computer wins!";
             computerScoreInt++;
             computerScore.textContent = computerScoreInt;
-            break
+            break;
         case 3:
             results.textContent = "Tie!";
-            break 
+            break;
     }
+
     if ((playerScoreInt == 5) || (computerScoreInt == 5)) {
-        // document.removeChild(buttonR);
-        // document.removeChild(buttonP);
-        // document.removeChild(buttonS);
-        let end = document.createElement("h2");
-        end.textContent = "Game is Over!";
-        cont.appendChild(end);
+        toggleDisable(1);
+        cont.appendChild(endText);
+        cont.appendChild(restartButton);
     }
-    //console.log(`Player Score: ${playerScore} Computer Score: ${computerScore}`)
 }
 
 function getComputerChoice(){
     let choice = Math.floor(Math.random()*3)
     switch(choice){
         case 0:
-            choice = "rock"
-            break
+            choice = "rock";
+            break;
         case 1: 
-            choice = "paper"
-            break
+            choice = "paper";
+            break;
         case 2:
-            choice = "scissors"
-            break
+            choice = "scissors";
+            break;
     }
+
     computerChoice.textContent = choice;
     return choice;
 }
 
 function decideWinner(playerSelection, computerSelection){
-    let winner = 0
+    let winner = 0;
+
     if (playerSelection == computerSelection) {
-        winner = 3
-        return winner
+        winner = 3;
+        return winner;
     }
 
     if (playerSelection == "rock"){
         if (computerSelection == "paper"){
-            winner = 2
-            return winner
+            winner = 2;
+            return winner;
         }else{
-            winner = 1
-            return winner
+            winner = 1;
+            return winner;
         }
     }
 
     if (playerSelection == "paper"){
         if (computerSelection == "rock"){
-            winner = 1
-            return winner
+            winner = 1;
+            return winner;
         }else{
-            winner = 2
-            return winner
+            winner = 2;
+            return winner;
         }
     }
 
     if (playerSelection == "scissors"){
         if (computerSelection == "paper"){
-            winner = 1
-            return winner
+            winner = 1;
+            return winner;
         }else{
-            winner = 2
-            return winner
+            winner = 2;
+            return winner;
+        }
+    }
+}
+
+function restart(){
+    toggleDisable(0);
+    cont.removeChild(endText);
+    cont.removeChild(restartButton);
+    init();
+}
+
+function toggleDisable(flag){
+    if (flag){
+        for (let button of theButtons){
+            button.setAttribute("disabled", "");
+        }
+    }else{
+        for (let button of theButtons){
+            button.removeAttribute("disabled");
         }
     }
 }
