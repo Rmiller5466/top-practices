2be28aaae8a99a073ef436d3a7ae474e007ea737
Ryan <ryan.miller5466@gmail.com>
2022-12-18 21:54:30 -0500

    Implements color picker
    - Size settings are now buttons
    - Labels are now included
    - Color picker implemented

--- foundations/drawing-board/index.js
+++ foundations/drawing-board/index.js
@@ -1,16 +1,19 @@
 let board = document.querySelector(".db");
-let gridToggle = document.querySelector("#grid-toggle");
-let reset = document.querySelector("#reset");
-
-let gridSizes = document.getElementsByName("grid-size");
-
-let boardCopy = board;
-
 let bWidth = board.offsetWidth - 4;
 let bHeight = board.offsetHeight - 4;
 
+
+let colorPicker = document.querySelector("#color-select");
+let drawingColor = colorPicker.value;
+
+let gridToggle = document.querySelector("#grid-toggle");
+
+let reset = document.querySelector("#reset");
+
+let gridSizes = document.getElementsByClassName("size-btn");
+
 function createGrid(numBoxes){
-    numBoxes = parseInt(numBoxes)
+    numBoxes = parseInt(numBoxes);
     let boxSize = bWidth / numBoxes;
     let totalBoxes = numBoxes * numBoxes;
 
@@ -19,21 +22,18 @@ function createGrid(numBoxes){
         let box = document.createElement("div");
         box.classList.add('square', "grid");
         box.addEventListener("mouseover", function() {
-            box.style.backgroundColor = "black";
+            box.style.backgroundColor = drawingColor;
         });
         box.style.width = `${boxSize}px`;
         box.style.height = `${boxSize}px`;
     
         board.appendChild(box);
-    };
+    }
+};
 
-}
-
-createGrid(16)
-
+createGrid(16);
 
 gridToggle.addEventListener('change', function() {
-    
     for (const child of board.children) {
         child.classList.toggle('grid');
     }
@@ -43,6 +43,10 @@ reset.addEventListener('click', function() {
     for (const child of board.children) {
         child.style.backgroundColor = "white";
     }
+});
+
+colorPicker.addEventListener('change', function(){
+    drawingColor = colorPicker.value;
 });
 
 for (let btn of gridSizes){
@@ -55,13 +59,5 @@ function updateGrid(newNum) {
     while (board.firstChild){
         board.removeChild(board.firstChild);
     }
-    createGrid(newNum)
-    // let val = 0;
-    // let elements = document.getElementsByName("grid-size");
-    // for (let element of elements){
-    //     if (element.checked){
-    //         val = element.value;
-    //     }
-    // }
-    // return val;
+    createGrid(newNum);
 }
